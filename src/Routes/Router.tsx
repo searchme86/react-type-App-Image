587b8187b06149c6e7a7d36ab18547152952768f
searchme86 <searchme86@gmail.com>
2022-01-31 01:54:32 +0900

    ⚡️ NotFound Page Added

--- src/Routes/Router.tsx
+++ src/Routes/Router.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import App from "../App";
 import Expenses from "../Views/Pages/Expenses";
 import Invoices from "../Views/Pages/Invoices";
+import NotFound from "../Views/Pages/NotFound";
 
 function Router() {
   return (
@@ -10,6 +11,7 @@ function Router() {
       <Route path="/" element={<App />}>
         <Route path="expense" element={<Expenses />} />
         <Route path="invoices" element={<Invoices />} />
+        <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
   );
