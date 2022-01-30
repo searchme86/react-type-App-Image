731bd820644b3c67e6e79507077b389c2331baac
searchme86 <searchme86@gmail.com>
2022-01-31 01:11:01 +0900

    🎉  App initial mockup done(Hello world)

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import GlobalStyle from "./Static/Styles/GlobalStyle";
 
 function App() {
   return (
     <div>
+      <GlobalStyle />
       <h1>hello world</h1>
     </div>
   );
